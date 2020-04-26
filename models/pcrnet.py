6d3164440e4b08d6cb0798ff83af46b34569a1db
vinits5 <vinitsarode5@gmail.com>
2020-04-26 14:49:44 -0400

    pcrnet.py model updated and spam module added

--- models/pcrnet.py
+++ models/pcrnet.py
@@ -24,30 +24,44 @@ class iPCRNet(nn.Module):
 
 		self.linear = nn.Sequential(*self.linear)
 
-	def forward(self, template, source, max_itr=8):
+	# Single Pass Alignment Module (SPAM)
+	def spam(self, template_features, source, est_R, est_t):
+		batch_size = source.size(0)
+
+		self.source_features = self.pooling(self.feature_model(source))
+		y = torch.cat([template_features, self.source_features], dim=1)
+		pose_7d = self.linear(y)
+		pose_7d = transform.create_pose_7d(pose_7d)
+
+		# Find current rotation and translation.
+		identity = torch.eye(3).to(source).view(1,3,3).expand(batch_size, 3, 3).contiguous()
+		est_R_temp = transform.quaternion_rotate(identity, pose_7d).permute(0, 2, 1)
+		est_t_temp = transform.get_translation(pose_7d).view(-1, 1, 3)
+
+		# update translation matrix.
+		est_t = torch.bmm(est_R_temp, est_t.permute(0, 2, 1)).permute(0, 2, 1) + est_t_temp
+		# update rotation matrix.
+		est_R = torch.bmm(est_R_temp, est_R)
+		
+		source = transform.quaternion_transform(source, pose_7d)      # Ps' = est_R*Ps + est_t
+		return est_R, est_t, source
+
+	def forward(self, template, source, max_iteration=8):
 		est_R = torch.eye(3).to(template).view(1, 3, 3).expand(template.size(0), 3, 3).contiguous()         # (Bx3x3)
 		est_t = torch.zeros(1,3).to(template).view(1, 1, 3).expand(template.size(0), 1, 3).contiguous()     # (Bx1x3)
-
 		template_features = self.pooling(self.feature_model(template))
 
-		# Iterations
-		for i in range(max_itr):
-			source_features = self.pooling(self.feature_model(source))
-			y = torch.cat([template_features, source_features], dim=1)
+		if max_iteration == 1:
+			est_R, est_t, source = self.spam(template_features, source, est_R, est_t)
+		else:
+			for i in range(max_iteration):
+				est_R, est_t, source = self.spam(template_features, source, est_R, est_t)
 
-			pose_7d = self.linear(y)
-			pose_7d = transform.create_pose_7d(pose_7d)
-
-			source = transform.quaternion_transform(source, pose_7d)      # Ps' = est_R*Ps + est_t
-			est_t = transform.quaternion_rotate(est_t, pose_7d) + transform.get_translation(pose_7d).view(-1, 1, 3)
-			est_R = transform.quaternion_rotate(est_R, pose_7d)
-
-		result = {'est_R': est_R,
-				  'est_t': est_t,
-				  'est_T': transform.convert2transformation(est_R, est_t),
-				  'r': template_features - source_features,
+		result = {'est_R': est_R,				# source -> template
+				  'est_t': est_t,				# source -> template
+				  'est_T': transform.convert2transformation(est_R, est_t),			# source -> template
+				  'r': template_features - self.source_features,
 				  'transformed_source': source}
-		
 		return result
 
 
