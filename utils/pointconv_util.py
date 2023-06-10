4420f0237c1380bbcf3e0a8316cc7a4d92af2174
vinits5 <vinit.s@karza.in>
2023-06-11 00:47:10 +0530

    kde to _kde due to sklearn version update

--- utils/pointconv_util.py
+++ utils/pointconv_util.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from time import time
 import numpy as np
-from sklearn.neighbors.kde import KernelDensity
+from sklearn.neighbors._kde import KernelDensity
 
 def timeit(tag, t):
     print("{}: {}s".format(tag, time() - t))
